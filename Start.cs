104d2cb5e7768e7b8abf186c526694e1a538ec5a
lythaiho <49936593+lythaiho@users.noreply.github.com>
2020-03-02 15:56:30 +0700

    1
    1

---
+++ Start.cs
@@ -0,0 +1,14 @@
+﻿namespace T1904a.Patical
+{
+    public class Start
+    {
+        public static void Main(string[] args)
+        {
+         
+            Phone tP = new Phone("DuyCode","Vinaphone",12345678);
+            MobilePhone Tmobile = new MobilePhone();
+            tP.outPut();
+            Tmobile.outPut();
+        }
+    }
+}
